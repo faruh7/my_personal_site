cffe44f54b7f78299b6833ccb6a017046ccedfe0
Farrukh <fsafarov34@gmail.com>
2022-04-21 01:53:15 +0300

    changed app into myapplication

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+myapplication = Flask(__name__)
 
 
-@app.route("/")
+@myapplication.route("/")
 def home_page():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    myapplication.run(debug=True)
